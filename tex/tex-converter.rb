893109b5079fa25e1e9a460d9a854bbe0f4d3098
James Carlson <jxxcarlson@mac.com>
2014-09-27 11:58:03 -0400

    Moved the colored text extensions of String class from tex-converter to a separate file

--- tex/tex-converter.rb
+++ tex/tex-converter.rb
@@ -49,8 +49,9 @@
 
 
 require 'asciidoctor'
-# require_relative 'tex_block'
-require '/Users/carlson/Dropbox/prog/git/asciidoctor-backends/tex/tex_block/'
+require_relative 'colored_text'
+require_relative 'tex_block'
+
 include TeXBlock
 
 class LaTeXConverter
@@ -155,48 +156,6 @@ class LaTeXConverter
   end
   
   
-end
-
-
-class String
-  
-  def abbreviate
-    self.split("\n")[0]
-  end
-  
-  
-  def blue
-    "\e[1;34m#{self}\e[0m"
-  end
-  
-  def green
-    "\e[1;32m#{self}\e[0m"
-  end
-
-  def red
-    "\e[1;31m#{self}\e[0m"
-  end
-  
-  def yellow
-    "\e[1;33m#{self}\e[0m"
-  end
-  
-  def magenta
-    "\e[1;35m#{self}\e[0m"
-  end
-  
-  def cyan
-    "\e[1;36m#{self}\e[0m"
-  end
-  
-  def white
-    "\e[1;37m#{self}\e[0m"
-  end
-  
-  def black
-    "\e[1;30m#{self}\e[0m"
-  end
-
 end
 
 
